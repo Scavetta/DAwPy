d72825dd4118a0570eb20e22b4bd44bde3512764
Boyan Angelov <boyan.angelov@gmail.com>
2018-08-15 16:49:04 +0300

    Add dfply plot

--- expression.py
+++ expression.py
@@ -2,12 +2,9 @@ import pandas as pd
 import numpy as np
 from dfply import *
 from scipy import stats
-import statsmodels.stats.api as sms
-
-sms.DescrStatsW(a).tconfint_mean()
-
+from statsmodels.formula.api import ols
+import seaborn as sns
 pd.set_option('display.max_columns', 500)
-
 
 expression_data = pd.read_csv('data/Expression.txt', delimiter='\t')
 expression_data.head()
@@ -30,3 +27,18 @@ df_summary = (df_t  >>
         group_by('gene', 'treatment', 'time') >>
         summarize_each([np.mean, np.std, len, stats.sem, mean_confidence_interval], 'value'))
 df_summary
+
+# TODO for one gene
+# (df_t  >>
+#     separate('key', ['treatment', 'gene', 'time'], remove=True) >>
+#     mask(X.gene == 'RIPK2') >>
+#     group_by(X.gene) >>
+#     ols("value ~ treatment", X))
+
+# TODO for all genes
+
+# make a plot
+df_temp = df_t  >> separate('key', ['treatment', 'gene', 'time'], remove=True)
+df_temp = df_temp.dropna()
+g = sns.FacetGrid(df_temp, col="treatment",  row="gene")
+g = g.map(sns.stripplot, "time", "value")
